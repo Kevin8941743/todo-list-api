ac2032e65936aa888650f71208197279d6ea905f
Kevin thapa <bobmarley9461238@gmail.com>
2026-04-17 14:46:05 +0100

    feat: provision EC2 instance with security group and SSH key

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,3 +1,20 @@
 provider "aws" {
     region = "eu-north-1"
 }
+
+resource "aws_instance" "todo-blog" {
+    tags = {
+        Name = "roadmap-api"
+    }
+
+    key_name="roadmap-api-crud"
+
+    vpc_security_group_ids = [aws_security_group.allow_tls.id]
+
+    ami = "ami-080254318c2d8932f"
+    instance_type = "t3.micro"
+}
+
+resource "aws_security_group" "allow_tls" {
+    name = "roadmap-security"
+}
