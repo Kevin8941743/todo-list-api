c2781734e9883c3ec296cb64cde591202ed4ef9a
Kevin thapa <bobmarley9461238@gmail.com>
2026-04-17 14:30:25 +0100

    chore: initialise express app with redis and postgres connection

--- app/server.js
+++ app/server.js
@@ -6,3 +6,21 @@ import pg from "pg"
 import bcrypt from "bcrypt"
 const { Pool } = pg
 
+dotenv.config()
+const PORT = 3001
+const app = express()
+app.use(express.json())
+const client = createClient({
+    url: process.env.REDIS_URL
+})
+
+const pool = new Pool({
+    user: process.env.DATABASE_USER,
+    host: process.env.DATABASE_HOST,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    port: process.env.DATABASE_PORT,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+})
